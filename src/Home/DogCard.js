0df79c647da4b350ea67b2f99b0d4b7a25c08003
thok-patrol <rswerner@wisc.edu>
2020-11-21 10:10:38 -0600

    Added Dog Modals, looks good

--- src/Home/DogCard.js
+++ src/Home/DogCard.js
@@ -1,5 +1,5 @@
 import react, { useEffect, useState } from 'react'
-import { Card, Image } from 'react-bootstrap'
+import { Card, Image, Modal } from 'react-bootstrap'
 
 function getCorg() {
     fetch('https://dog.ceo/api/breed/pembroke/images/random')
@@ -15,6 +15,7 @@ function getCorg() {
 export default function DogCard(props) {
     
     const [dogLink, updateLink] = useState('')
+    const [show, setShow] = useState(false)
 
     useEffect(() => {
         fetch('https://dog.ceo/api/breed/pembroke/images/random')
@@ -26,8 +27,20 @@ export default function DogCard(props) {
     }, [])
 
     return <div className='m-3 shadow dogCardParent'>
-        <Image variant='top' className='dogCard'  src={dogLink}>
+        <Image variant='top' className='dogCard'  src={dogLink} onClick={() => {setShow(true)}}>
         </Image>
         <p className='dogCardName'>Flufferbottom</p>
+
+    <Modal show={show} onHide={() => setShow(false)}>
+        <div>
+            <Image src={dogLink} width='100%' />
+        </div>
+        <div className='m-3'>
+            <h3>Luna</h3>
+            <small className='text-muted'><p>Pembroke Welsh Corgi owned by Bailey C.</p></small>
+            <p>Look at this cute pupper go. She loves to zoom around the yard when Bailey takes her outside. She also loves long walks along the Fox River.</p>
+        </div>
+    </Modal>
+
     </div>
 }
